f43abf450c9a0fbeda5bd3a2d1daeb7baa008923
Felipe Siervi <felipe.siervi@gmail.com>
2020-03-29 22:13:49 -0300

    ...

--- compras.py
+++ compras.py
@@ -15,11 +15,27 @@ class compras:
     @staticmethod
     def get_nota_itens(id):
         dbpg = pgdb()
-        print("id {}".format(id))
-        rows = dbpg.query(
-            "select di.iddocumentoitem, di.iddetalhe, di.qtitem, di.vlunitario, di.vlsubst, di.vlipi, de.dsdetalhe "
-            + "from wshop.docitem di "
-            + "join wshop.detalhe de on di.iddetalhe = de.iddetalhe "
-            + "where di.iddocumento = '{}'".format(id)
+        qry = "select di.iddocumentoitem, di.iddetalhe, di.qtitem, di.vlunitario, di.vlsubst/di.qtitem vlsubst, di.vlipi/di.qtitem vlipi, de.dsdetalhe, de.allucrodesejada, de.vlprecovenda vlprecoprazo, dp.vlpreco vlprecovista, di.vlfreterateado, 0 vlrateio \
+            from wshop.docitem di \
+            join wshop.detalhe de on di.iddetalhe = de.iddetalhe \
+            join wshop.detalheprecos dp on dp.iddetalhe = de.iddetalhe \
+            where di.iddocumento = '{}' \
+            and idtabela = '01000EBK0Y'".format(
+            id
         )
+        rows = dbpg.query(qry)
         return rows.to_json(orient="records")
+
+    @staticmethod
+    def post_preco(obj):
+        dbpg = pgdb()
+        update_prazo = "update wshop.detalhe set vlprecovenda = {} where iddetalhe = '{}'".format(
+            obj["prazo"], obj["id"]
+        )
+        update_avista = "update wshop.detalheprecos set vlpreco = {} where iddetalhe = '{}'".format(
+            obj["avista"], obj["id"]
+        )
+
+        dbpg.execute(update_avista)
+        dbpg.execute(update_prazo)
+        return {"message": "Preços atualizados com sucesso"}
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from compras import compras
 from flask.wrappers import Response
 from flask_cors import CORS, cross_origin
 from flask import request
+import json
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "the quick brown fox jumps over the lazy   dog"
@@ -11,17 +12,21 @@ app.config["CORS_HEADERS"] = "Content-Type"
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-@cross_origin(origin="localhost", headers=["Content- Type", "Authorization"])
 @app.route("/compras/get_notas")
 def get_notas():
     return Response(compras.get_notas(), mimetype="application/json")
 
 
-@cross_origin(origin="localhost", headers=["Content- Type", "Authorization"])
 @app.route("/compras/get_nota_itens")
 def get_nota_itens():
     id = request.args.get("id")
     return Response(compras.get_nota_itens(id), mimetype="application/json")
+
+
+@app.route("/compras/post_preco", methods=["POST"])
+def post_preco():
+    obj = json.loads(request.data)
+    return compras.post_preco(obj)
 
 
 if __name__ == "__main__":
